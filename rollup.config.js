4ed53b94785433e962654b4196b9b6fc7f9c1f14
Djamel Hassaine <dhassaine@gmail.com>
2018-10-22 08:19:57 +0200

    Preact bindings for reduxless

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import json from "rollup-plugin-json";
+import minify from "rollup-plugin-babel-minify";
+import typescript from "rollup-plugin-typescript2";
+
+const base = {
+  external: ["preact"]
+};
+
+const plugins = [
+  json(),
+  resolve(),
+  typescript({
+    useTsconfigDeclarationDir: true,
+    tsconfig: "tsconfig.json"
+  }),
+  babel({
+    exclude: ["node_modules/**"],
+    plugins: ["external-helpers"]
+  })
+];
+
+export default [
+  {
+    ...base,
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "cjs"
+    },
+    sourcemap: true,
+    plugins
+  },
+
+  {
+    ...base,
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.esm.js",
+      format: "es"
+    },
+    sourcemap: true,
+    plugins
+  }
+];
